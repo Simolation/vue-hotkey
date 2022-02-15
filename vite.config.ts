2527c48b719224c1089093eb0ff3d2e8037dd3b9
Simon Osterlehner <simon@osterlehner.de>
2022-02-16 00:16:18 +0100

    Initialize base vue and vite library

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "VueHotkey",
+      fileName: (format) => `vue-hotkey.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["vue"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+  // Opt-out vue-demi pre-bundling
+  optimizeDeps: {
+    exclude: ["vue-demi"],
+  },
+});
